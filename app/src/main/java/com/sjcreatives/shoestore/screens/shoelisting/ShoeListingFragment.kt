54983bfb3c9806069a893730f8aedac3daea6c96
samueljuma <samueljuma1024@gmail.com>
2024-03-23 01:47:22 +0300

    add menu for shoe_list screen

--- app/src/main/java/com/sjcreatives/shoestore/screens/shoelisting/ShoeListingFragment.kt
+++ app/src/main/java/com/sjcreatives/shoestore/screens/shoelisting/ShoeListingFragment.kt
@@ -3,12 +3,21 @@ package com.sjcreatives.shoestore.screens.shoelisting
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.onNavDestinationSelected
 import com.sjcreatives.shoestore.MyViewModel
+import com.sjcreatives.shoestore.R
 import com.sjcreatives.shoestore.data.Shoe
 import com.sjcreatives.shoestore.databinding.FragmentShoeListingBinding
 import com.sjcreatives.shoestore.databinding.ShoeItemLayoutBinding
@@ -26,17 +35,38 @@ class ShoeListingFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentShoeListingBinding.inflate(layoutInflater, container,false)
 
+      //  setHasOptionsMenu(true) // This is Deprecated as of 3/23/2024
+
+        // Handle FAB click events
         binding.addFAB.setOnClickListener {
             findNavController().navigate(
                 ShoeListingFragmentDirections.actionShoeListingFragmentToShoeDetailFragment()
             )
         }
+
         //Observe livedata for list of shoes and display
         myViewModel.listOfShoes.observe(viewLifecycleOwner){shoes ->
             shoes?.let {
                 displayShoes(shoes)
             }
         }
+
+        /**
+         * This code handles the Manu Item click
+         */
+        val menuProvider = object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.shoe_list_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return NavigationUI.onNavDestinationSelected(menuItem, requireView().findNavController())
+            }
+
+        }
+        requireActivity().addMenuProvider(menuProvider,viewLifecycleOwner, Lifecycle.State.RESUMED)
+        //-----------------------------------------------------------------------------------------------
+
 
         return binding.root
     }
